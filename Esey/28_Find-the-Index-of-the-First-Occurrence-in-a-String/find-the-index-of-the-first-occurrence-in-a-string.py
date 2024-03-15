cc21d45a73a19ee6e3d33a38c775c4473a367b22
cotoro-lab <q3o3p.zz@gmail.com>
2024-03-15 14:10:36 +0900

    28. Find the Index of the First Occurrence in a String

---
+++ Esey/28_Find-the-Index-of-the-First-Occurrence-in-a-String/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,16 @@
+# 28. Find the Index of the First Occurrence in a String
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        hs_list = list(haystack)
+        chk_str = ""
+        rtn = -1
+        for i in range(0, len(hs_list)):
+            if len(hs_list) >= i + len(needle)-1:
+                chk_str = "".join(hs_list[i:i + len(needle)])
+                if chk_str == needle:
+                    rtn = i
+                    break
+
+        # 一番早かった処理
+        # rtn = haystack.find(needle)
+        return rtn
